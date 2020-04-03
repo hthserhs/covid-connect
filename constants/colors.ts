4bf5e5c7c713341701b61326dce4af0117ae0027
Nishant Shreshth <nshreshth@vmware.com>
2020-04-04 02:13:56 +0530

    Fix app.json

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,4 +1,4 @@
-export const C1 = '#00aeef'; // rgb(0, 174, 239) primary
+export const C1 = '#62c4c3'; // rgb(98, 196, 195) primary
 export const C2 = '#00c000'; // rgb(0, 192, 0)
 export const C3 = '#ffd600'; // rgb(255, 214, 0)
 export const C4 = '#ff9900'; // rgb(255, 153, 0)
@@ -8,4 +8,4 @@ export const C7 = '#ebebeb'; // rgb(235, 235, 235)
 export const C8 = '#dddddd'; // rgb(221, 221, 221)
 export const C9 = '#979797'; // rgb(151, 151, 151)
 export const C10 = '#393939'; // rgb(57, 57, 57)
-export const C11 = '#f9aa33'; // rgb(249, 170, 51) secondary
+export const C11 = '#efbc2e'; // rgb(239, 188, 46) secondary
